d449851bbfcc9dcde0c158b2c2851051041a9c8d
Andrei Dmitritsenkov <andrei.dmitritsenkov@gmail.com>
2019-09-08 15:01:11 +0300

    Beta 1.0.1 (Built)

---
+++ build/precache-manifest.30f2358d40fc193a4364b4adf6e8f01f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2b3b9c67365035f4148ff8d82a46cc",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "7bceedaa597fcbc41e75",
+    "url": "/todoList/static/css/main.7535ed9f.chunk.css"
+  },
+  {
+    "revision": "7f7e1c02c8c3dfaed08a",
+    "url": "/todoList/static/js/2.de8f6704.chunk.js"
+  },
+  {
+    "revision": "7bceedaa597fcbc41e75",
+    "url": "/todoList/static/js/main.f78cd3cc.chunk.js"
+  },
+  {
+    "revision": "5fd5f1b880b0caddb620",
+    "url": "/todoList/static/js/runtime~main.6fc37664.js"
+  }
+]);
